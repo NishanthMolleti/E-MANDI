9f075e5db24bf0798c5f113ea855fa2f8595618e
NishanthMolleti <48158364+NishanthMolleti@users.noreply.github.com>
2020-05-19 20:25:07 +0530

    farmer functionality added

--- src/main/java/model/StakeHoldersLogin.java
+++ src/main/java/model/StakeHoldersLogin.java
@@ -1,14 +1,27 @@
 package model;
 import java.io.IOException;
-import java.util.HashMap;
+
 import java.util.Scanner;
 import service.ExcelReader;
-
-public class StakeHoldersLogin 
+class PostLogin
+{
+	static void farmer()
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.print("Enter name of the crop you wanna sell:");
+		String crop=s.nextLine();
+		System.out.println("\n");
+		System.out.println("Enter the quantity of crop you produced(in kgs) : ");
+		int  quan=s.nextInt();
+		System.out.println("Enter the base price at which you wanna sell you crop (per kg):");
+		int base_f=s.nextInt();
+	}
+}
+public class StakeHoldersLogin extends PostLogin
 {
 	public static void main(String[] args) throws IOException 
 	{
-		HashMap<String ,String > h=new HashMap<String ,String>();
+
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter Your Email : ");
 		
@@ -16,14 +29,27 @@ public class StakeHoldersLogin
 		System.out.print("Enter Your Password : ");
 		
 		String pass = sc.nextLine();
-		h.put(email , pass );
+
 		ExcelReader vc = new ExcelReader();
 		System.out.println("\n1:Farmer\n2.Wholesaler\n3.Retailer\n4.Civilian\nEnter your profession :");
 		int prof=sc.nextInt();
-		if(vc.validateCredentials(email,pass,prof)) 
+		if(vc.validateCredentials(email,pass)) 
 		{
 			System.out.println("Logged in Successfully");
-			//bidding code
+			switch(prof)
+			{
+			case 1: System.out.println("Congo!!! You are Logged in as a Farmer");
+					farmer();
+				break;
+			case 2:System.out.println("Congo!!! You are Logged in as a Wholesaler");
+				break;
+			case 3:System.out.println("Congo!!! You are Logged in as a Retailer");
+				break;
+			case 4:System.out.println("Congo!!! You are Logged in as a Civilian");
+				break;
+			default:System.out.println("please try logging in again !");
+			
+			}
 		}
 		else 
 		{
